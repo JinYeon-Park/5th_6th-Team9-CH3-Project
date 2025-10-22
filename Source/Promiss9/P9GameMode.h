2793931f947648571ef8686ae35a0821a6da3094
JinYeon-Park <a9526191@gmail.com>
2025-10-22 12:38:02 +0900

    add P9GameState, BP_P9GameState, P9GameMode
    add : P9GameState, P9GameMode, BP_P9GameState 의 .h, .cpp파일 생성 및 초기 세팅 작성

---
+++ Source/Promiss9/P9GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "P9GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROMISS9_API AP9GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
