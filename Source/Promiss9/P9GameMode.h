2383e156d447cd58a68e3bd8711a46632809fb1c
JinYeon-Park <a9526191@gmail.com>
2025-10-22 16:07:57 +0900

    update :  P9GameState 참조용 포인터 선언
    P9GameMode.h에 P9GameState를 참조할 포인터변수 선언

--- Source/Promiss9/P9GameMode.h
+++ Source/Promiss9/P9GameMode.h
@@ -1,8 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
+#include "UObject/ObjectPtr.h"
+#include "P9GameState.h"
 #include "GameFramework/GameModeBase.h"
 #include "P9GameMode.generated.h"
 
@@ -13,5 +15,9 @@ UCLASS()
 class PROMISS9_API AP9GameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+private:
+
+	UPROPERTY()
+	TObjectPtr<AP9GameState> P9GameState;
 };
